99eb69b7d1301483de9aee4f65369d49cac83b77
Isaac Yocum <isyocum@gmail.com>
2018-08-04 16:13:05 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    ResumeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([  {
+      path: '',
+      component: HomeComponent
+    },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+      {
+        path: 'resume',
+        component: ResumeComponent
+      }
+      ]),
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
